0bb237b6166d3be9784aa51833da4a7ae60426e5
NishanWijethunga <nishansameerawijethunga@gmail.com>
2020-05-14 03:36:00 +0530

    app shell + pwa

--- apps/front-end/src/app/app.module.ts
+++ apps/front-end/src/app/app.module.ts
@@ -2,10 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    }),
+    RouterModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
