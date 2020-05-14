1646af4cb47dfd109c35095bdb28e9a8b8c936a7
NishanWijethunga <nishansameerawijethunga@gmail.com>
2020-05-14 05:45:02 +0530

    environments updated.

--- apps/front-end/src/environments/environment.ts
+++ apps/front-end/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC4G3VjRZTCWDdZp3Q445Pt5eYyWL0eMao',
+    authDomain: 'pelaguru-bb41c.firebaseapp.com',
+    databaseURL: 'https://pelaguru-bb41c.firebaseio.com',
+    projectId: 'pelaguru-bb41c',
+    storageBucket: 'pelaguru-bb41c.appspot.com',
+    messagingSenderId: '320834074626',
+    appId: '1:320834074626:web:ebd8578725813317124f1b',
+    measurementId: 'G-11XNHR40GV'
+  }
 };
 
 /*
