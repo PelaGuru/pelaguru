5d5a73af8c24001ed4e43758c42ba5f554d78a8c
NishanWijethunga <nishansameerawijethunga@gmail.com>
2020-05-14 07:59:38 +0530

    firebase initialized.

--- apps/front-end/src/app/core/core.module.ts
+++ apps/front-end/src/app/core/core.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { environment } from '../../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule.enablePersistence(),
+    AngularFireStorageModule
   ]
 })
 export class CoreModule { }
--- apps/front-end/src/environments/environment.ts
+++ apps/front-end/src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyC4G3VjRZTCWDdZp3Q445Pt5eYyWL0eMao',
-    authDomain: 'pelaguru-bb41c.firebaseapp.com',
-    databaseURL: 'https://pelaguru-bb41c.firebaseio.com',
-    projectId: 'pelaguru-bb41c',
-    storageBucket: 'pelaguru-bb41c.appspot.com',
-    messagingSenderId: '320834074626',
-    appId: '1:320834074626:web:ebd8578725813317124f1b',
-    measurementId: 'G-11XNHR40GV'
+    apiKey: "AIzaSyCNwKyB7gfQgwUGVJYsIIKennbmghqf0MU",
+    authDomain: "pelaguru-dev.firebaseapp.com",
+    databaseURL: "https://pelaguru-dev.firebaseio.com",
+    projectId: "pelaguru-dev",
+    storageBucket: "pelaguru-dev.appspot.com",
+    messagingSenderId: "41926452209",
+    appId: "1:41926452209:web:27c755cceed4f0d43f9a50",
+    measurementId: "G-H7SEM57MHS"
   }
 };
 
