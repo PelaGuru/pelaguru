c298cde2e35eef52901edd8a529e48e2e45ba8c7
NishanWijethunga <nishansameerawijethunga@gmail.com>
2020-05-20 13:28:56 +0530

    updated.

--- apps/back-end/src/main.ts
+++ apps/back-end/src/main.ts
@@ -1,12 +1,12 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as firebaseAdmin from 'firebase-admin';
-import { apiRouter } from './app/routes/api-routes';
+import { router } from './app/routes';
 
 firebaseAdmin.initializeApp();
 
 const app = express();
 
-app.use('/api', apiRouter);
+app.use('/', router);
 
 export const api = functions.https.onRequest(app);
