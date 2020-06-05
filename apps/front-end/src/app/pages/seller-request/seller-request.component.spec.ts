39fbb2fb2f3f8024cb3514fbc9502f47318819e7
Nishan Wijethunga <nishansameerawijethunga@gmail.com>
2020-06-05 19:44:10 +0530

    seller request component added.

---
+++ apps/front-end/src/app/pages/seller-request/seller-request.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SellerRequestComponent } from './seller-request.component';
+
+describe('SellerRequestComponent', () => {
+  let component: SellerRequestComponent;
+  let fixture: ComponentFixture<SellerRequestComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [SellerRequestComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SellerRequestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ apps/front-end/src/app/pages/seller-request/seller-request.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pelaguru-seller-request',
+  templateUrl: './seller-request.component.html',
+  styleUrls: ['./seller-request.component.scss']
+})
+export class SellerRequestComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
