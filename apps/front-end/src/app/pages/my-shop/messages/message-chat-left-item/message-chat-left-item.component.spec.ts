1b3e9e04a268cd7c4c7b59617a381f3d6547312a
Nishan Wijethunga <nishansameerawijethunga@gmail.com>
2020-06-26 04:23:16 +0530

    message chat right, left item components created.

---
+++ apps/front-end/src/app/pages/my-shop/messages/message-chat-left-item/message-chat-left-item.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MessageChatLeftItemComponent } from './message-chat-left-item.component';
+
+describe('MessageChatLeftItemComponent', () => {
+  let component: MessageChatLeftItemComponent;
+  let fixture: ComponentFixture<MessageChatLeftItemComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [MessageChatLeftItemComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MessageChatLeftItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
