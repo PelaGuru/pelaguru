b887950855d465287c23b1aaf4a9b203692ea15f
Nishan Wijethunga <nishansameerawijethunga@gmail.com>
2020-06-25 18:02:30 +0530

    vendor dashboard behavior chart generated.

---
+++ apps/front-end/src/app/pages/my-shop/dashboard/charts/behavior-chart/behavior-chart.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BehaviorChartComponent } from './behavior-chart.component';
+
+describe('BehaviorChartComponent', () => {
+  let component: BehaviorChartComponent;
+  let fixture: ComponentFixture<BehaviorChartComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [BehaviorChartComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BehaviorChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
