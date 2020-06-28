59a54112fdb5a165d3dbd85c937f76c834291703
pathum <awpathum@gmail.com>
2020-06-28 16:52:04 +0530

    footer done

--- apps/front-end/src/app/shared/footer/footer.component.ts
+++ apps/front-end/src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavLinkData } from '../models/nav-link-data';
 
 @Component({
   selector: 'pelaguru-footer',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  navLinks: Array<NavLinkData> = [
+    { label: 'Home', link: '/home' },
+    { label: 'Plants', link: '/plants' },
+    { label: 'Diseases', link: '/diseases' },
+    { label: 'Marketplace', link: '/marketplace' },
+    { label: 'My Shop', link: '/my-shop/dashboard' },
+  ];
+
+  socialLinks:Array<NavLinkData> =[
+      {label:'About Us',link:'/aboutUs'}
+  ]
+
+
   constructor() {}
 
   ngOnInit(): void {}
