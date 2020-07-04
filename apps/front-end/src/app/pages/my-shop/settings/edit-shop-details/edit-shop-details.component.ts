c33e72dfe33d3df9d0e3f5c4e840e3f53721b21d
Nishan Wijethunga <nishansameerawijethunga@gmail.com>
2020-07-04 12:58:19 +0530

    vendor shop settings module created.

---
+++ apps/front-end/src/app/pages/my-shop/settings/edit-shop-details/edit-shop-details.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pelaguru-edit-shop-details',
+  templateUrl: './edit-shop-details.component.html',
+  styleUrls: ['./edit-shop-details.component.scss']
+})
+export class EditShopDetailsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ apps/front-end/src/app/pages/my-shop/settings/settings-list/shop-visibility/shop-visibility.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pelaguru-shop-visibility',
+  templateUrl: './shop-visibility.component.html',
+  styleUrls: ['./shop-visibility.component.scss']
+})
+export class ShopVisibilityComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
