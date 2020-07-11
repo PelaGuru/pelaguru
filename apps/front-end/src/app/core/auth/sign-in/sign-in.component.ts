4825c479633e56e30dd6b32488437d4b83604c78
induwarajayalath <induwarajayalath@gmail.com>
2020-07-11 15:32:41 +0530

    siginin OKAY

--- apps/front-end/src/app/core/auth/sign-in/sign-in.component.ts
+++ apps/front-end/src/app/core/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { AuthService } from '../auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pelaguru-sign-in',
@@ -6,7 +11,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
-  constructor() {}
+  formController: FormGroup;
+  matcher: ErrorStateMatcher;
+
+  constructor(
+    private authService: AuthService,
+    private snackbar: MatSnackBar,
+    private router: Router
+  ) {
+    this.formController = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+    this.matcher = new ErrorStateMatcher();
+  }
 
   ngOnInit(): void {}
+
+  async signIn() {
+    if (this.formController.valid) {
+      const response = await this.authService.signInWithEmailPassword(
+        this.formController.get('email').value,
+        this.formController.get('password').value
+      );
+      if (response.success) {
+        this.router.navigate(['/']);
+      } else {
+        if (response.message === 'FIREAUTH_ERROR') {
+          if (response.error.code === 'auth/user-not-found') {
+            this.snackbar.open(
+              'Your are not created a account. Please try to Sign Up.',
+              'close'
+            );
+          } else {
+            this.snackbar.open(
+              'Something went wrong. Try again later.',
+              'close'
+            );
+          }
+        } else {
+          this.snackbar.open('Something went wrong. Try again later.', 'close');
+        }
+      }
+    }
+  }
 }
