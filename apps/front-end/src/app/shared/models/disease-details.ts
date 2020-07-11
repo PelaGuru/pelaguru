eb062851cac3159afd130835468f4d97836b55cc
pathum <awpathum@gmail.com>
2020-07-11 23:49:41 +0530

    new models added

---
+++ apps/front-end/src/app/shared/models/disease-details.ts
@@ -0,0 +1,10 @@
+export interface DiseaseDetails {
+  diseaseName: string;
+  symptoms: string[];
+  causativeAgent: string;
+  additionalFeatures: string;
+  chemicals: string;
+  commonPlants: string[];
+  image: string;
+  desc: string;
+}
