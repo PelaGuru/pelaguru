3b8dfd44c66eb8f86357211839312d842d854822
pathum <awpathum@gmail.com>
2020-07-11 23:52:31 +0530

    new interfaces added

--- libs/interfaces/src/index.ts
+++ libs/interfaces/src/index.ts
@@ -9,6 +9,12 @@ export * from './lib/common/service-response';
 // Plants
 export * from './lib/plants/plant-catalogue-item';
 
+//diseases
+export * from './lib/diseases/disease-catalogue-item';
+
 // My Shop
 /// Messaging
 export * from './lib/my-shop/messaging/message-chat-container-data';
+
+//chemical
+export * from './lib/chemical/chemical-suggestion-item';
