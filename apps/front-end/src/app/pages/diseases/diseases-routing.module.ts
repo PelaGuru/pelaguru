68d20b70c51e3d9fd84badc73e107f122f076a32
pathum <awpathum@gmail.com>
2020-07-11 23:47:38 +0530

    routing added

--- apps/front-end/src/app/pages/diseases/diseases-routing.module.ts
+++ apps/front-end/src/app/pages/diseases/diseases-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DiseasesComponent } from './diseases.component';
 import { DiseasesCatalogueComponent } from './diseases-catalogue/diseases-catalogue.component';
+import { ViewDiseaseComponent } from './view-disease/view-disease.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DiseasesComponent,
-    children: [{ path: '', component: DiseasesCatalogueComponent }]
+    children: [
+      { path: '', component: DiseasesCatalogueComponent },
+      { path: ':plantUsername', component: ViewDiseaseComponent }
+    ]
   }
 ];
 
--- apps/front-end/src/app/pages/diseases/diseases.module.ts
+++ apps/front-end/src/app/pages/diseases/diseases.module.ts
@@ -5,9 +5,14 @@ import { DiseasesRoutingModule } from './diseases-routing.module';
 import { DiseasesComponent } from './diseases.component';
 import { DiseasesCatalogueComponent } from './diseases-catalogue/diseases-catalogue.component';
 import { SharedModule } from '../../shared/shared.module';
+import { ViewDiseaseComponent } from './view-disease/view-disease.component';
 
 @NgModule({
-  declarations: [DiseasesComponent, DiseasesCatalogueComponent],
+  declarations: [
+    DiseasesComponent,
+    DiseasesCatalogueComponent,
+    ViewDiseaseComponent
+  ],
   imports: [CommonModule, DiseasesRoutingModule, SharedModule]
 })
 export class DiseasesModule {}
