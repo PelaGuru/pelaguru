afa8daf9525905203d47c6590c89be5fe252d1c1
Nishan Wijethunga <nishansameerawijethunga@gmail.com>
2020-08-24 17:08:45 +0530

    error codes library added.

---
+++ libs/error-codes/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'error-codes',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/libs/error-codes'
+};
---
+++ libs/error-codes/src/lib/auth.ts
@@ -0,0 +1,9 @@
+export enum AuthErrorCodes {
+  InternalError = 'auth:internal-error',
+  EmailAlreadyExists = 'auth:email-already-exists',
+  InvalidCredential = 'auth:invalid-credential',
+  InvalidArgument = 'auth:invalid-argument',
+  InvalidEmail = 'auth:invalid-email',
+  InvalidPassword = 'auth:invalid-password',
+  UserNotFound = 'auth:user-not-found'
+}
