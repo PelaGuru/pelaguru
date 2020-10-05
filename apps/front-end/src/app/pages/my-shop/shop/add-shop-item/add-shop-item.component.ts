b65618b430260c1dbf2a21f02abee6ece746bb0f
Lahiruka Wijesinghe <2017is098@stu.ucsc.cmb.ac.lk>
2020-10-05 16:46:13 +0530

    add-item

--- apps/front-end/src/app/pages/my-shop/shop/add-shop-item/add-shop-item.component.ts
+++ apps/front-end/src/app/pages/my-shop/shop/add-shop-item/add-shop-item.component.ts
@@ -1,12 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'pelaguru-add-shop-item',
   templateUrl: './add-shop-item.component.html',
   styleUrls: ['./add-shop-item.component.scss'],
 })
 export class AddShopItemComponent implements OnInit {
-  constructor() {}
+  isLinear = false;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  title = 'dropzone';
+  files: File[] = [];
+  constructor(private _formBuilder: FormBuilder, private http: HttpClient) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required],
+    });
+  }
+
+  onSelect(event) {
+    console.log(event);
+    this.files.push(...event.addedFiles);
+  }
+
+  onRemove(event) {
+    console.log(event);
+    this.files.splice(this.files.indexOf(event), 1);
+  }
 }
