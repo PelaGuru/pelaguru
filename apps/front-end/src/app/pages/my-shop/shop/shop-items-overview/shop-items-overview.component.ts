e3080b5988fbe10b558e98dcd4e4b40527c590d5
Nishan Wijethunga <nishansameerawijethunga@gmail.com>
2020-10-07 10:03:09 +0530

    shop items added.

--- apps/front-end/src/app/pages/my-shop/shop/shop-items-overview/shop-items-overview.component.ts
+++ apps/front-end/src/app/pages/my-shop/shop/shop-items-overview/shop-items-overview.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Shop, ShopItem } from '@pelaguru/interfaces';
+import { BehaviorSubject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { AuthService } from '../../../../core/auth/auth.service';
 
 @Component({
   selector: 'pelaguru-shop-items-overview',
@@ -6,7 +11,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop-items-overview.component.scss'],
 })
 export class ShopItemsOverviewComponent implements OnInit {
-  constructor() {}
+  items: BehaviorSubject<ShopItem[]> = new BehaviorSubject([]);
+  constructor(
+    private authService: AuthService,
+    private fireStore: AngularFirestore
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getAllItems()
+  }
+
+  getAllItems() {
+    this.authService
+      .getUserStream()
+      .pipe(
+        switchMap(async (response) => {
+          const data = await this.fireStore.firestore
+            .collection('Shops')
+            .where('vendorId', '==', response.userId)
+            .get();
+          const shopId = (data.docs[0].data() as Shop).id;
+          const dataRef = this.fireStore
+            .collection('Shops')
+            .doc(shopId)
+            .collection('items');
+          const docs = await dataRef.get().toPromise();
+          if (docs.empty) {
+            this.items.next([]);
+          } else {
+            const docsData = docs.docs.map((d) => {
+              return d.data() as ShopItem;
+            });
+            this.items.next(docsData);
+          }
+        })
+      )
+      .subscribe();
+  }
 }
