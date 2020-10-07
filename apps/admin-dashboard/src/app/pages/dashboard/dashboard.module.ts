db88984bf5d610d8526e30fda832452a8402a0de
Nishan Wijethunga <nishansameerawijethunga@gmail.com>
2020-10-07 10:56:37 +0530

    miner fix

--- apps/admin-dashboard/src/app/pages/dashboard/dashboard.module.ts
+++ apps/admin-dashboard/src/app/pages/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardRoutingModule],
+  imports: [CommonModule, DashboardRoutingModule, SharedModule],
 })
 export class DashboardModule {}
